caa47eeb4515f33755143588d761d3f95c05af17
邱胜明 <qiushengming@mofanghr.com>
2016-11-07 19:17:08 +0800

    Update project

--- library/src/main/java/com/mc/library/network/okhttp/HTTPCallClient.java
+++ library/src/main/java/com/mc/library/network/okhttp/HTTPCallClient.java
@@ -1,17 +1,12 @@
 package com.mc.library.network.okhttp;
 
-import com.google.gson.Gson;
-
 /**
  * Created by dinghui on 2016/10/27.
  */
 public class HTTPCallClient {
     public static void execute() {
-        Gson gson = new Gson();
-        gson.fromJson("", int.class);
     }
 
     public static void enqueue() {
-
     }
 }
